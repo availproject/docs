e7d59abbc827233b358c6eaabfc4a4c7fc63b47c
Priyank Gupta <priyankgupta0308@gmail.com>
2025-01-31 17:46:48 +0530

    user-guides section done

--- app/user-guides/_meta.ts
+++ app/user-guides/_meta.ts
@@ -1,4 +1,14 @@
 export default {
-    accounts: "Get started with an Avail wallet"
+    "accounts": "Get started with an Avail wallet",
+    "avail-multisig": "Set up a multisig on Avail",
+    "avail-proxy": "Set up a proxy on Avail",
+    "identity": "Set an on-chain identity",
+    "avail-snap": "Use Avail with MetaMask",
+    "ledger-avail": "Use a ledger wallet on Avail",
+    "vectorx": "Bridge AVAIL to Ethereum",
+    "staking-governance": "Staking & Governance on Avail",
+    "avail-funds-recovery": {
+        "display": "hidden"
+      }
 }
 
