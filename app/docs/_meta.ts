af75aa221dc532e1ec3248495fd7d44f253e097c
Priyank Gupta <priyankgupta0308@gmail.com>
2025-01-31 20:14:09 +0530

    docs section top-level pages done

--- app/docs/_meta.ts
+++ app/docs/_meta.ts
@@ -1,4 +1,17 @@
 export default {
-    "welcome-to-avail-docs": "Welcome to Avail Docs"
-}
+        "welcome-to-avail-docs": "Start Here!",
+    
+        "clash-of-nodes": {
+            "display": "hidden"
+        },
+        "networks": "Network Info",
+        "build-with-avail": "Build with Avail",
+        "learn-about-avail": "Learn more about Avail",
+        "operate-a-node": "Run a node",
+        "glossary": "Glossary",
+        "faqs": "FAQs",
+        "archived-content": {
+            "display": "hidden"
+        }
+    }
 
