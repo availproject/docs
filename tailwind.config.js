3aaea328809a766bb90de9a01ed22b1f2ac16a86
Christine <christine@availproject.org>
2025-02-06 11:13:01 -1000

    fix css for landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,33 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-      './app/**/*.{js,jsx,ts,tsx,md,mdx}',
-      './content/**/*.{md,mdx}',
-   
-      // Or if using `src` directory:
-      './src/**/*.{js,jsx,ts,tsx,md,mdx}'
-    ],
-    theme: {
-      extend: {}
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './app/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './components/**/*.{js,jsx,ts,tsx,md,mdx}',
+    './content/**/*.{md,mdx}',
+    './node_modules/nextra-theme-docs/**/*.{js,jsx,ts,tsx,md,mdx}'
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      noHover: {
+        '&:hover': {
+          background: 'initial',
+          color: 'inherit',
+        },
+      },
+      fontFamily: {
+        'thicccboisemibold': ['Thicccboi-semibold', 'sans-serif'],
+        'thicccboibold': ['Thicccboi-bold', 'sans-serif'],
+        'thicccboiregular': ['Thicccboi-regular', 'sans-serif'],
+        'ppmori': ['PP Mori', 'sans-serif'],
+      },
     },
-    plugins: []
-  }
+  },
+  plugins: [],
+}
+
