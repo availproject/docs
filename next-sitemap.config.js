d5d2ac74a2dc9895fcb2f983ceab788b7535a12a
Priyank Gupta <priyankgupta0308@gmail.com>
2025-02-24 21:47:59 +0530

    fixing sitemap file

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,1 +1,15 @@
 /** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://docs.availproject.org',
+  generateRobotsTxt: true,
+  changefreq: 'daily',
+  priority: 0.7,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+      }
+    ]
+  }
+}
