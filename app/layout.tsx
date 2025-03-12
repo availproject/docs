2bdaf2c103a794ae991891cce23ad213d75f355a
Abheek Tripathy <abheek.tripathy@gmail.com>
2025-03-13 02:59:13 +0530

    force image data

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,18 @@
-import { Layout, Navbar } from 'nextra-theme-docs'
+import { Layout, Navbar } from "nextra-theme-docs";
 import { Footer } from "@components/Footer/Footer";
-import { Head, Search } from 'nextra/components'
+import { Head, Search } from "nextra/components";
 import { Metadata } from "next";
 import { ReactNode } from "react";
-import { getPageMap } from 'nextra/page-map'
-import 'nextra-theme-docs/style.css'
-import './globals.css'
-import { CustomNavbar } from '@components/Navbar';
-import { sharedMetadata } from '@components/lib/metadata';
+import { getPageMap } from "nextra/page-map";
+import "nextra-theme-docs/style.css";
+import "./globals.css";
+import { CustomNavbar } from "@components/Navbar";
+import { sharedMetadata } from "@components/lib/metadata";
 
+export const metadata: Metadata = sharedMetadata;
 
-export const metadata: Metadata = sharedMetadata
+const footer = <Footer />;
 
-const footer = <Footer/>
- 
 export default async function RootLayout({
   children,
 }: {
@@ -21,37 +20,41 @@ export default async function RootLayout({
 }) {
   return (
     <html lang="en" dir="ltr" suppressHydrationWarning>
-      <Head  
-    />
+      <Head>
+        <meta property="og:image" content="/img/docs-link-preview.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <meta name="twitter:image" content="/img/docs-link-preview.png" />
+      </Head>
       <body>
         <Layout
           navbar={
             <Navbar
-              logo={
-                <CustomNavbar/>
-              }
+              logo={<CustomNavbar />}
               projectLink="https://github.com/availproject/docs"
               chatLink="https://twitter.com/AvailProject"
               chatIcon={
                 <svg width="24" height="24" viewBox="0 0 248 204">
                   <path
-                      fill="currentColor"
-                      d="M221.95 51.29c.15 2.17.15 4.34.15 6.53 0 66.73-50.8 143.69-143.69 143.69v-.04c-27.44.04-54.31-7.82-77.41-22.64 3.99.48 8 .72 12.02.73 22.74.02 44.83-7.61 62.72-21.66-21.61-.41-40.56-14.5-47.18-35.07a50.338 50.338 0 0 0 22.8-.87C27.8 117.2 10.85 96.5 10.85 72.46v-.64a50.18 50.18 0 0 0 22.92 6.32C11.58 63.31 4.74 33.79 18.14 10.71a143.333 143.333 0 0 0 104.08 52.76 50.532 50.532 0 0 1 14.61-48.25c20.34-19.12 52.33-18.14 71.45 2.19 11.31-2.23 22.15-6.38 32.07-12.26a50.69 50.69 0 0 1-22.2 27.93c10.01-1.18 19.79-3.86 29-7.95a102.594 102.594 0 0 1-25.2 26.16z"
-                    />
-                  </svg>
-                }
+                    fill="currentColor"
+                    d="M221.95 51.29c.15 2.17.15 4.34.15 6.53 0 66.73-50.8 143.69-143.69 143.69v-.04c-27.44.04-54.31-7.82-77.41-22.64 3.99.48 8 .72 12.02.73 22.74.02 44.83-7.61 62.72-21.66-21.61-.41-40.56-14.5-47.18-35.07a50.338 50.338 0 0 0 22.8-.87C27.8 117.2 10.85 96.5 10.85 72.46v-.64a50.18 50.18 0 0 0 22.92 6.32C11.58 63.31 4.74 33.79 18.14 10.71a143.333 143.333 0 0 0 104.08 52.76 50.532 50.532 0 0 1 14.61-48.25c20.34-19.12 52.33-18.14 71.45 2.19 11.31-2.23 22.15-6.38 32.07-12.26a50.69 50.69 0 0 1-22.2 27.93c10.01-1.18 19.79-3.86 29-7.95a102.594 102.594 0 0 1-25.2 26.16z"
+                  />
+                </svg>
+              }
             />
           }
-          
           navigation={{
             prev: true,
-            next: true
-          }}  
-          
+            next: true,
+          }}
           pageMap={await getPageMap()}
           docsRepositoryBase="https://github.com/availproject/docs/tree/main"
           editLink="Edit this page on GitHub"
-          sidebar={{ defaultMenuCollapseLevel: 1, autoCollapse: true, toggleButton: true }}
+          sidebar={{
+            defaultMenuCollapseLevel: 1,
+            autoCollapse: true,
+            toggleButton: true,
+          }}
           footer={footer}
           search={
             <Search
