9ca7ee9ff9498e1553fb9b8d58f596755b9022b2
Priyank Gupta <priyankgupta0308@gmail.com>
2025-03-12 23:47:05 +0530

    chore: removing redundant files & images

--- app/docs/layout.tsx
+++
@@ -1,8 +0,0 @@
-import { sharedMetadata } from '@components/lib/metadata';
-import { ReactNode } from 'react';
-
-export const metadata = sharedMetadata;
-
-export default function Layout({ children }: { children: ReactNode }) {
-  return children;
-}
