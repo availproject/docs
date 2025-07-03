f66bca4fe0118e7a5cbc5ac1cc9a6818f78f746b
Priyank Gupta <priyankgupta0308@gmail.com>
2025-07-04 00:28:11 +0530

    chore: minor update to a page

--- app/api-reference/avail-nexus-sdk/examples/_meta.ts
+++ app/api-reference/avail-nexus-sdk/examples/_meta.ts
@@ -1,5 +1,6 @@
 export default {
 
+    "fetch-unified-balances": "Fetch unified balance using the Nexus SDK",
     "transfer": "Transfer tokens using the Nexus SDK",
     "bridge-tokens": "Bridge tokens using the Nexus SDK",
     "bridge-and-execute": "Bridge & Execute with the Nexus SDK"
