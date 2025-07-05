7c402355cd6bb7c5118fe2295c5d3bc778a3af75
Priyank Gupta <priyankgupta0308@gmail.com>
2025-07-05 16:37:53 +0530

    chore: removing some outdate docs

--- app/docs/build-with-avail/avail-nexus/_meta.ts
+++
@@ -1,6 +0,0 @@
-export default {
-    
-    "overview": "Avail Nexus Overview",
-    "avail-nexus-tutorial": "Avail Nexus Mailbox Examples",
-    "avail-nexus-architecture": "Avail Nexus Architecture"
-}
