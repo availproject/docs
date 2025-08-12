7c1592e0f6fa29d4b5b1671171d2ad51b61e1a69
Priyank Gupta <priyankgupta0308@gmail.com>
2025-08-12 17:09:15 +0200

    chore: made progress on overview page update

--- app/api-reference/avail-nexus-sdk/_meta.ts
+++ app/api-reference/avail-nexus-sdk/_meta.ts
@@ -2,6 +2,6 @@ export default {
 
     "overview": "Get Started",
     "nexus-sdk-core": "Core SDK",
-    "nexus-sdk-ui": "UI Components",
+    "nexus-sdk-ui": "UI Widgets",
     "api-reference": "API Reference",
 }
