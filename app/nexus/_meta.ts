4deb3b983ad948983bd95df67596b63466d642d3
Priyank Gupta <priyankgupta0308@gmail.com>
2025-08-27 20:18:19 +0530

    chore: added concepts directory

--- app/nexus/_meta.ts
+++ app/nexus/_meta.ts
@@ -2,6 +2,5 @@ export default {
     "introduction-to-nexus": "Introduction to Nexus",
     "concepts": "Concepts",
     "avail-nexus-sdk": "Nexus SDK Reference",
-    "faqs": "FAQs"
 }
 
---
+++ app/nexus/concepts/_meta.ts
@@ -0,0 +1,7 @@
+export default {
+    "chain-abstraction": "Chain Abstraction",
+    "unified-balance": "Unified Balance",
+    "allowances": "Allowances",
+    "intent": "Intent",
+    "solvers": "Solvers"
+}
