7467fd3b2eea21309d0334ec7e5f39677bb4134f
Priyank Gupta <priyankgupta0308@gmail.com>
2025-09-17 23:18:53 +0530

    chore: updated core API reference

--- app/nexus/avail-nexus-sdk/examples/nexus-core/_meta.ts
+++ app/nexus/avail-nexus-sdk/examples/nexus-core/_meta.ts
@@ -2,5 +2,6 @@ export default {
     "fetch-unified-balances": "Unified Balance",
     "transfer": "Transfer",
     "bridge-tokens": "Bridge",
-    "bridge-and-execute": "Bridge & Execute"
+    "bridge-and-execute": "Bridge & Execute",
+    "api-reference": "API Reference"
 }
