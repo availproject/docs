2490d6cb5bf85dea448afbc86a7d56f1a3c7a01d
Priyank Gupta <priyankgupta0308@gmail.com>
2025-10-10 22:47:24 +0530

    feat: Made minor updates, Nexus SDK updated docs ready to roll out

--- app/nexus/nexus-examples/_meta.ts
+++ app/nexus/nexus-examples/_meta.ts
@@ -1,6 +1,6 @@
 export default {
     "nexus-initialization-basic": "Barebones initialization inside Next JS",
-    "nexus-initialization-rainbowkit": "Add RainbowKit to Your Nexus SDK Project",
+    "nexus-initialization-rainbowkit": "Initialize the Nexus SDK using RainbowKit",
     // "nexus-porto-extension": "Add Porto Wallet to Your Nexus SDK Project",
-//    "nexus-bridge": "Bridge tokens using the Nexus SDK",
+    //    "nexus-bridge": "Bridge tokens using the Nexus SDK",
 }
