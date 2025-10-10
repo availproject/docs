c9ca574a622403855607cdab085d7957f17d077b
robin-rrt <66296664+robin-rrt@users.noreply.github.com>
2025-10-10 19:39:06 +0530

    add rainbowkit guide

--- app/nexus/nexus-examples/_meta.ts
+++ app/nexus/nexus-examples/_meta.ts
@@ -1,6 +1,6 @@
 export default {
     "nexus-initialization-basic": "Barebones initialization inside Next JS",
-    "nexus-porto-extension": "Add Porto Wallet to Your Nexus SDK Project",
     "nexus-rainbowkit-extension": "Add RainbowKit to Your Nexus SDK Project",
+    // "nexus-porto-extension": "Add Porto Wallet to Your Nexus SDK Project",
 //    "nexus-bridge": "Bridge tokens using the Nexus SDK",
 }
