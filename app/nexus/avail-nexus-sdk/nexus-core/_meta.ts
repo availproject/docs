8eb5999584b4c9d439c33de1ae481c51a37664a9
Priyank Gupta <priyankgupta0308@gmail.com>
2025-10-18 15:55:12 +0530

    chore: added xcs for nexus-core to staging docs

--- app/nexus/avail-nexus-sdk/nexus-core/_meta.ts
+++ app/nexus/avail-nexus-sdk/nexus-core/_meta.ts
@@ -3,5 +3,6 @@ export default {
     "transfer": "Transfer",
     "bridge": "Bridge",
     "bridge-and-execute": "Bridge & Execute",
+    "swap": "Swap",
     "api-reference": "API Reference"
 }
--- app/nexus/avail-nexus-sdk/nexus-widgets/_meta.ts
+++ app/nexus/avail-nexus-sdk/nexus-widgets/_meta.ts
@@ -2,5 +2,6 @@ export default {
     "transfer": "Transfer",
     "bridge": "Bridge",
     "bridge-and-execute": "Bridge & Execute",
+    "swap": "Swap",
     "api-reference": "API Reference"
 }
