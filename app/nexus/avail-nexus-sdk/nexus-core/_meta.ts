db559a75516cef0b64a2ee456daab8981147f306
Priyank Gupta <priyankgupta0308@gmail.com>
2025-11-12 16:01:00 +0400

    chore: updated bridge & bridgeAndTransfer pages

--- app/nexus/avail-nexus-sdk/nexus-core/_meta.ts
+++ app/nexus/avail-nexus-sdk/nexus-core/_meta.ts
@@ -1,7 +1,7 @@
 export default {
     "fetch-unified-balances": "Fetch Unified Balance",
-    "transfer": "Transfer",
     "bridge": "Bridge",
+    "transfer": "Bridge and Transfer",
     "bridge-and-execute": "Bridge & Execute",
     "swap-exact-in": "Swap Exact In",
     "swap-exact-out": "Swap Exact Out",
