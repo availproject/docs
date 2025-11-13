f8bfa1d651fffdddaf40e103483bd06fb120d670
Priyank Gupta <priyankgupta0308@gmail.com>
2025-11-13 11:27:11 +0400

    chore: progress dump, lots of stuff still left to do

--- app/nexus/nexus-examples/_meta.ts
+++ app/nexus/nexus-examples/_meta.ts
@@ -1,6 +1,7 @@
 export default {
     "nexus-initialization-basic": "Barebones initialization inside Next JS",
     "nexus-initialization-rainbowkit": "Initialize the Nexus SDK using RainbowKit",
+    "bridge-execute": "Bridge and execute a transaction using the Nexus SDK",
     // "nexus-porto-extension": "Add Porto Wallet to Your Nexus SDK Project",
     //    "nexus-bridge": "Bridge tokens using the Nexus SDK",
 }
