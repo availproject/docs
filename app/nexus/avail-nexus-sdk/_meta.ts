565150d7c94df26c77881f83a6f9c7e6d5170013
Priyank Gupta <priyankgupta0308@gmail.com>
2025-11-19 19:00:35 +0530

    fix: fixed a few things

--- app/nexus/avail-nexus-sdk/_meta.ts
+++ app/nexus/avail-nexus-sdk/_meta.ts
@@ -2,5 +2,4 @@ export default {
     "nexus-core": "Nexus Core",
     "nexus-elements": "Nexus Elements",
     "hooks-and-errors": "Hooks and Errors",
-    "common-utils-and-constants": "Common Utils and Constants",
 }
