d2ed4e5ecdbbcd829f83ec4cc925e9e1cb485e93
sukanyaparashar <sukanyaparashar111@gmail.com>
2025-11-25 23:19:18 +0530

    Add bridge functionality

--- app/nexus/nexus-examples/_meta.ts
+++ app/nexus/nexus-examples/_meta.ts
@@ -1,9 +1,11 @@
 export default {
-    "nexus-initialization-basic": "Barebones initialization inside Next JS",
-    "nexus-initialization-rainbowkit": "Initialize the Nexus SDK using RainbowKit",
-    "bridge-and-transfer": "Bridge and transfer tokens using the Nexus SDK",
-    "bridge-execute": "Bridge and execute a transaction using the Nexus SDK",
+  "nexus-initialization-basic": "Barebones initialization inside Next JS",
+  "nexus-initialization-rainbowkit":
+    "Initialize the Nexus SDK using RainbowKit",
+  "nexus-bridge": "Bridge tokens using the Nexus SDK",
+  "bridge-and-transfer": "Bridge and transfer tokens using the Nexus SDK",
+  "bridge-execute": "Bridge and execute a transaction using the Nexus SDK",
 
-    // "nexus-porto-extension": "Add Porto Wallet to Your Nexus SDK Project",
-    //    "nexus-bridge": "Bridge tokens using the Nexus SDK",
-}
+  // "nexus-porto-extension": "Add Porto Wallet to Your Nexus SDK Project",
+  //    "nexus-bridge": "Bridge tokens using the Nexus SDK",
+};
