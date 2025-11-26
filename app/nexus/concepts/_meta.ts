3f7da4e25eb274123280f159991db5e43f5f05e3
Priyank Gupta <priyankgupta0308@gmail.com>
2025-11-26 22:33:41 +0530

    feat: urgent push for mainnet launch

--- app/nexus/concepts/_meta.ts
+++ app/nexus/concepts/_meta.ts
@@ -1,13 +1,11 @@
 export default {
     "chain-abstraction": "Chain Abstraction",
+    "allowances": "Allowances",
     "intent": "Intent",
     "solvers": "Solvers",
     "intent-lifecycle": "Intent Lifecycle",
-    "nexus-ops": "What is Bridge, Transfer, Bridge & Execute",
-    "xcs-swaps": "Crosschain Swaps",
+    "nexus-ops": "Bridge Functions",
+    "xcs-swaps": "Swap Functions",
     "source-chain-selection": "Selecting source chains for Nexus operations",
     "bridge-v-swap": "Bridge balances vs Swap balances",
-    "allowances": "Allowances"
-
-
 }
